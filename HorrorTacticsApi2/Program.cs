ba108490b6443ee102b179c79dac7d31cb191f48
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2022-01-06 15:26:28 -0600

    Add serilog

--- HorrorTacticsApi2/Program.cs
+++ HorrorTacticsApi2/Program.cs
@@ -4,80 +4,110 @@ using Jonwolfdev.Utils6.Auth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("./logs/ht-logs-.txt", rollingInterval: RollingInterval.Day, fileSizeLimitBytes: 5242880, retainedFileCountLimit: 10)
+    .WriteTo.File("./logs/ht-errors-init-.txt", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning)
+    .CreateBootstrapLogger();
 
-builder.Configuration.AddEnvironmentVariables(prefix: Constants.ENV_PREFIX);
-// Add services to the container.
+Log.Information("HorrorTactics starting up...");
 
-builder.Services.AddOptions<AppSettings>()
-    .Bind(builder.Configuration.GetSection(Constants.APPSETTINGS_GENERAL_KEY))
-    .ValidateDataAnnotations();
-{ 
-    // Jwt setup
-    builder.Services.AddOptions<JwtGeneratorStaticOptions>()
-        .Bind(builder.Configuration.GetSection(Constants.APPSETTINGS_JWTGENERATOR_KEY))
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) =>
+    {
+        lc.WriteTo.Console()
+            .ReadFrom.Configuration(ctx.Configuration);
+    });
+
+    builder.Configuration.AddEnvironmentVariables(prefix: Constants.ENV_PREFIX);
+    // Add services to the container.
+
+    builder.Services.AddOptions<AppSettings>()
+        .Bind(builder.Configuration.GetSection(Constants.APPSETTINGS_GENERAL_KEY))
         .ValidateDataAnnotations();
-
-    builder.Services.AddAuthentication(auth =>
     {
-        auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        // Jwt setup
+        builder.Services.AddOptions<JwtGeneratorStaticOptions>()
+            .Bind(builder.Configuration.GetSection(Constants.APPSETTINGS_JWTGENERATOR_KEY))
+            .ValidateDataAnnotations();
+
+        builder.Services.AddAuthentication(auth =>
+        {
+            auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        });
+
+        builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtBearerOptions>();
+    }
+
+    builder.Services.AddDbContext<HorrorDbContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString(Constants.CONNECTION_STRING_MAIN_KEY))
+    );
+
+    builder.Services
+        .AddControllers()
+        .AddNewtonsoftJson();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddCors(cors =>
+    {
+        cors.AddDefaultPolicy(policy =>
+        {
+            policy.AllowAnyOrigin();
+            policy.AllowAnyMethod();
+            policy.AllowAnyHeader();
+        });
     });
 
-    builder.Services.AddSingleton<IConfigureOptions<JwtBearerOptions>, ConfigureJwtBearerOptions>();
-}
+    var app = builder.Build();
 
-builder.Services.AddDbContext<HorrorDbContext>(options => 
-    options.UseSqlite(builder.Configuration.GetConnectionString(Constants.CONNECTION_STRING_MAIN_KEY))
-);
-
-builder.Services
-    .AddControllers()
-    .AddNewtonsoftJson();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(cors =>
-{
-    cors.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin();
-        policy.AllowAnyMethod();
-        policy.AllowAnyHeader();
-    });
-});
+        // TODO: make sure this is not called when replacing services in API integration
+        using var scope2 = app.Services.CreateScope();
+        // Making sure the database file is always updated
+        var db = scope2.ServiceProvider.GetRequiredService<HorrorDbContext>();
+        await db.Database.MigrateAsync();
+    }
 
-var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        // TODO: what is the difference of having devexception page and not having it?
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
+    app.UseCors();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseSerilogRequestLogging();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    // TODO: make sure this is not called when replacing services in API integration
-    using var scope2 = app.Services.CreateScope();
-    // Making sure the database file is always updated
-    var db = scope2.ServiceProvider.GetRequiredService<HorrorDbContext>();
-    await db.Database.MigrateAsync();
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("HorrorTactics shutdown.");
+    Log.CloseAndFlush();
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    // TODO: what is the difference of having devexception page and not having it?
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors();
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.MapControllers();
-
-app.Run();
 
 // This has to be added so it can be used within public classes
 namespace HorrorTacticsApi2
