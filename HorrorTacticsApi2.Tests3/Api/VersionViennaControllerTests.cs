4ab199ad60072bc4cf2fd6bead005b062c4a853e
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2022-10-06 19:59:40 -0500

    version vienna

---
+++ HorrorTacticsApi2.Tests3/Api/VersionViennaControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using HorrorTacticsApi2.Domain.Models;
+using HorrorTacticsApi2.Tests3.Api.Helpers;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HorrorTacticsApi2.Tests3.Api
+{
+    public class VersionViennaControllerTests : IClassFixture<ApiTestsCollection>
+    {
+        readonly ApiTestsCollection _factory;
+        const string Path = "/VersionVienna";
+        public VersionViennaControllerTests(ApiTestsCollection factory)
+        {
+            _factory = factory;
+            _factory.WebAppFactory.Options.DbName = nameof(VersionViennaControllerTests);
+        }
+
+        [Fact]
+        public async Task Should_Return_Text()
+        {
+            // arrange
+            using var client = _factory.CreateClient();
+            
+            // act
+            var response = await client.GetAsync(Path);
+            var text = await response.Content.ReadAsStringAsync();
+
+            // assert
+            Assert.Contains("Date: ", text);
+        }
+
+    }
+}
---
+++ HorrorTacticsApi2/Controllers/VersionViennaController.cs
@@ -0,0 +1,32 @@
+﻿using HorrorTacticsApi2.Domain;
+using HorrorTacticsApi2.Domain.Dtos;
+using HorrorTacticsApi2.Domain.Models;
+using Jonwolfdev.Utils6.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Mime;
+using System.Security.Claims;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HorrorTacticsApi2.Controllers
+{
+    [AllowAnonymous]
+    [Route("[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class VersionViennaController
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Produces(MediaTypeNames.Text.Plain)]
+        public ActionResult<string> Get()
+        {
+            const string version = "1.2";
+            return $"v{version} - Date: {DateTime.Now}";
+        }
+    }
+}
